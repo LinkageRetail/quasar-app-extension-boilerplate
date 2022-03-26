3de828ccdbbe494c2d5ca08dba9c29a522360b84
robby <ta7382@gmail.com>
2022-03-26 22:16:39 +0800

    Upgrade quasar.config.js

--- ui/dev/quasar.conf.js
+++ ui/dev/quasar.conf.js
@@ -1,21 +1,30 @@
 // Configuration for your app
 // https://quasar.dev/quasar-cli/quasar-conf-js
 
+const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = function (ctx) {
   return {
+    // https://v1.quasar.dev/quasar-cli/supporting-ts
+    supportTS: false,
+
+    // https://v1.quasar.dev/quasar-cli/prefetch-feature
+    // preFetch: true,
+
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
+    // https://v1.quasar.dev/quasar-cli/boot-files
     boot: ['register.js'],
 
+    // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [],
 
+    // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
       // 'mdi-v4',
-      'fontawesome-v5',
+      // 'fontawesome-v5',
       // 'eva-icons',
       // 'themify',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
@@ -23,33 +32,55 @@ module.exports = function (ctx) {
       'material-icons', // optional, you are not bound to it
       'material-icons-outlined',
       'material-icons-round',
+      'material-icons-sharp',
     ],
 
-    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework、
+    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
+    build: {
+      vueRouterMode: 'history',
+      // transpile: false,
+
+      // Add dependencies for transpiling with Babel (Array of string/regex)
+      // (from node_modules, which are by default not transpiled).
+      // Applies only if "transpile" is set to true.
+      // transpileDependencies: [],
+
+      // rtl: false, // https://v1.quasar.dev/options/rtl-support
+      // preloadChunks: true,
+      // showProgress: false,
+      // gzip: true,
+      // analyze: true,
+
+      // Options below are automatically set depending on the env, set them if you want to override
+      // extractCSS: false,
+
+      // https://v1.quasar.dev/quasar-cli/handling-webpack
+      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
+      chainWebpack(chain) {
+        chain.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
+        chain.resolve.alias.merge({
+          ui: path.resolve(__dirname, `../src/index.esm.js`),
+        });
+      },
+    },
+
+    // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
+    devServer: {
+      https: false,
+      port: 8080,
+      open: true, // opens browser window automatically
+    },
+
+    // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      // iconSet: 'ionicons-v4', // Quasar icon set
-      // lang: 'de', // Quasar language pack
-
-      // Possible values for "all":
-      // * 'auto' - Auto-import needed Quasar components & directives
-      //            (slightly higher compile time; next to minimum bundle size; most convenient)
-      // * false  - Manually specify what to import
-      //            (fastest compile time; minimum bundle size; most tedious)
-      // * true   - Import everything from Quasar
-      //            (not treeshaking Quasar; biggest bundle size; convenient)
-      all: 'auto',
-
-      components: [],
-      directives: [],
-
-      // Quasar plugins
-      plugins: ['Notify', 'Dialog', 'Loading', 'LoadingBar', 'LocalStorage', 'SessionStorage'],
+      iconSet: 'material-icons', // Quasar icon set
+      lang: 'en-us', // Quasar language pack
       config: {
         notify: {
           type: 'positive',
           position: 'top-right',
+          timeout: 1500,
           group: false,
-          timeout: 2000,
         },
         loading: {
           /* Loading defaults */
@@ -65,36 +96,37 @@ module.exports = function (ctx) {
           bodyClasses: true,
         },
       },
+
+      // Possible values for "importStrategy":
+      // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
+      // * 'all'  - Manually specify what to import
+      importStrategy: 'auto',
+
+      // https://v1.quasar.dev/layout/grid/introduction-to-flexbox
+      cssAddon: true,
+
+      // For special cases outside of where "auto" importStrategy can have an impact
+      // (like functional components as one of the examples),
+      // you can manually specify Quasar components/directives to be available everywhere:
+      //
+      // components: [],
+      // directives: [],
+
+      // Quasar plugins
+      plugins: [
+        'Notify',
+        'Dialog',
+        'Loading',
+        'LoadingBar',
+        'LocalStorage',
+        'SessionStorage',
+        'Cookies',
+      ],
     },
 
-    // https://quasar.dev/options/animations
-    animations: 'all', // --- includes all animations
-    // animations: [],
-
-    // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
-    supportIE: true,
-
-    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
-    build: {
-      vueRouterMode: 'history',
-
-      chainWebpack(chain) {
-        chain.resolve.alias.merge({
-          ui: path.resolve(__dirname, `../src/index.esm.js`),
-        });
-
-        chain.plugin('define-ui').use(webpack.DefinePlugin, [
-          {
-            __UI_VERSION__: `'${require('../package.json').version}'`,
-          },
-        ]);
-      },
-    },
-
-    devServer: {
-      // port: 8080,
-      open: true, // opens browser window automatically
-    },
+    // animations: 'all', // --- includes all animations
+    // https://v1.quasar.dev/options/animations
+    animations: 'all',
 
     ssr: {
       middlewares: [
