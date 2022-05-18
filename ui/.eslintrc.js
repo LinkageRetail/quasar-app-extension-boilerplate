d55904759ffd474b9708057ba5a8c82f44601ac3
robby <ta7382@gmail.com>
2022-05-19 04:54:38 +0800

    Update eslint

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -77,9 +77,12 @@ module.exports = {
     // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
 
+    '@typescript-eslint/no-empty-function': 'off',
+
     '@typescript-eslint/no-explicit-any': 'off',
     // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
     '@typescript-eslint/no-var-requires': 'off',
+
 
     // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
     // does not work with type definitions
