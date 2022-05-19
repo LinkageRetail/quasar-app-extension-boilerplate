d08f780e72429c5b46823434a06a249fbc9f47ad
annielin-linkage <annie.lin@linkage-retail.com>
2022-05-19 14:42:35 +0800

    Fix FieldDateTime

--- ui/src/components/Input/FieldDateTime.vue
+++ ui/src/components/Input/FieldDateTime.vue
@@ -1,12 +1,21 @@
 <template>
   <!-- This input date use for filter or search bar -->
   <q-input
+    ref="ref"
+    dense
+    outlined
+    :filled="filled"
+    :disable="disable"
+    :readonly="readonly"
+    :mask="mask"
+    :label="label"
+    :rules="rulesData"
     :clearable="clearable"
     :value="value"
     @change="handleChange"
     @clear="handleClear"
-    dense
-    outlined
+    v-on="$listeners"
+    v-bind="[$attrs, $props]"
   >
     <template #prepend>
       <q-icon :name="icon" class="cursor-pointer">
@@ -63,9 +72,29 @@ export default {
       type: [Number, String, Date],
       default: '',
     },
+    label: {
+      type: String,
+      required: true,
+    },
+    filled: {
+      type: Boolean,
+      default: true,
+    },
     mask: {
       type: String,
       default: 'datetime',
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
     },
     clearable: {
       type: Boolean,
@@ -95,10 +124,16 @@ export default {
       type: [Array, Function, String, Date],
       default: date => date,
     },
+    rules: {
+      type: Array,
+      default: () => [val => !!val],
+    },
   },
   data() {
     return {
       model: this.value, // Inner value for q-date
+      rulesData: this.required ? this.rules : [],
+      hasError: _.has(this.$refs['ref'], 'hasError') ? this.$refs['ref'].hasError : false,
     };
   },
   methods: {
@@ -117,6 +152,9 @@ export default {
     },
     parseSubTitle(value) {
       return value ? date.formatDate(value, this.formatSubTitle) : ' ';
+    },
+    validate() {
+      return _.isFunction(this.$refs['ref'].validate) && this.$refs['ref'].validate();
     },
   },
 };
